a4eb3759650ffd1339f04ad163a72e28f33e5a39
Linh Pham <linhmaipham@usf.edu>
2025-03-30 16:08:25 -0400

    Task 2 - SalesRep Class

--- cc_17.js
+++ cc_17.js
@@ -15,3 +15,21 @@ class Customer {
     }
 }
 
+
+// Task 2: Create a SalesRep Class
+class SalesRep {
+    constructor(name) {
+        this.name = name; // name (string)
+        this.clients = []; // clients (array of Customer objects)
+    }
+
+    addClient(customer) { // adds a customer to the list
+        this.clients.push(customer);
+    }
+
+    getClientTotal(name) { // finds client by name and returns total spent
+        const client = this.clients.find(client => client.name === name); 
+        return client ? client.getTotalSpent() : 0;
+    }
+}
+
